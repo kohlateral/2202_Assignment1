a676f55ac8693f54f5cf5514ab40271fbf4e258a
Koh Cheng Kiat <kohchengkiat@gmail.com>
2022-10-31 21:26:01 +0800

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from Anti_Forensics_Toolkit import secure_delete as sd
+from Anti_Forensics_Toolkit import forensic_artifact_shredder as fas
+import sys
 
 
-# Press the green button in the gutter to run the script.
+def menu():
+    print("WARNING: This program will permanently delete files you specify.")
+    print("1. Secure Delete")
+    print("2. Delete Prefetch Files")
+    print("3. Exit")
+    choice = input("Enter choice: ")
+    if choice == "1":
+        file_path = input("Enter absolute file path: ")
+        confirm = input("Proceed with secure delete of " + file_path + "? (y/n)\n")
+        if confirm == "y":
+            sd.secure_delete(file_path)
+            print("File Deleted\n")
+            menu()
+        else:
+            menu()
+    elif choice == "2":
+        confirm = input("Proceed with secure delete of prefetch files? (y/n)\n")
+        if confirm == "y":
+            fas.delete_prefetch_files()
+            print("Prefetch Files Deleted\n")
+            menu()
+        else:
+            menu()
+        menu()
+    elif choice == "3":
+        sys.exit()
+    else:
+        print("Invalid choice\n")
+        menu()
+
+
+def main():
+    menu()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
