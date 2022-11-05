adfe307b0946c1dddc2d62147918a4680aab06b1
Koh Cheng Kiat <kohchengkiat@gmail.com>
2022-11-05 20:45:59 +0800

    Fixed USB overwrite_usb_registry_key_identification()

--- Anti_Forensics_Toolkit/forensic_USB_artifact_shredder.py
+++ Anti_Forensics_Toolkit/forensic_USB_artifact_shredder.py
@@ -21,35 +21,33 @@ def overwrite_usb_registry_key_identification():
         # print all subkeys
         for i in range(0, winreg.QueryInfoKey(reg_key)[0]):
             device = winreg.EnumKey(reg_key, i)
-            list_of_devices.append(device)
+            list_of_devices.append(r"SYSTEM\CurrentControlSet\Enum\USBSTOR" + "\\" + device)
             # list all subkeys
-            for device in list_of_devices:
-                subkey = r"SYSTEM\CurrentControlSet\Enum\USBSTOR" + "\\" + device
-                open_subkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, subkey, 0, winreg.KEY_ALL_ACCESS)
-                for j in range(0, winreg.QueryInfoKey(open_subkey)[0]):
-                    serial_number = winreg.EnumKey(open_subkey, j)
-                    list_of_serial_numbers.append(serial_number)
+        for device in list_of_devices:
+            open_subkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, device, 0, winreg.KEY_ALL_ACCESS)
+            for j in range(0, winreg.QueryInfoKey(open_subkey)[0]):
+                serial_number = winreg.EnumKey(open_subkey, j)
+                list_of_serial_numbers.append(device + "\\" + serial_number)
                 # query values
-                for serial_number in list_of_serial_numbers:
-                    subkey = subkey + "\\" + serial_number
-                    open_subkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, subkey, 0, winreg.KEY_ALL_ACCESS)
-                    for k in range(0, winreg.QueryInfoKey(open_subkey)[1]):
-                        usb_information = winreg.EnumValue(open_subkey, k)
-                        try:
-                            # if data type is REG_SZ
-                            if usb_information[2] == 1:
-                                winreg.SetValueEx(open_subkey, usb_information[0], 0,
-                                                  usb_information[2], zero_REG_SZ_value)
-                            # if data type is REG_DWORD
-                            elif usb_information[2] == 4:
-                                winreg.SetValueEx(open_subkey, usb_information[0], 0,
-                                                  usb_information[2], zero_REG_DWORD_value)
-                            # if data type is REG_MULTI_SZ
-                            elif usb_information[2] == 7:
-                                winreg.SetValueEx(open_subkey, usb_information[0], 0,
-                                                  usb_information[2], zero_REG_MULTI_SZ_value)
-                        except Exception as e:
-                            print('Error ' + usb_information[0] + ' ' + str(e))
+        for serial_number in list_of_serial_numbers:
+            open_subkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, serial_number, 0, winreg.KEY_ALL_ACCESS)
+            for k in range(0, winreg.QueryInfoKey(open_subkey)[1]):
+                usb_information = winreg.EnumValue(open_subkey, k)
+                try:
+                    # if data type is REG_SZ
+                    if usb_information[2] == 1:
+                        winreg.SetValueEx(open_subkey, usb_information[0], 0,
+                                          usb_information[2], zero_REG_SZ_value)
+                    # if data type is REG_DWORD
+                    elif usb_information[2] == 4:
+                        winreg.SetValueEx(open_subkey, usb_information[0], 0,
+                                          usb_information[2], zero_REG_DWORD_value)
+                    # if data type is REG_MULTI_SZ
+                    elif usb_information[2] == 7:
+                        winreg.SetValueEx(open_subkey, usb_information[0], 0,
+                                          usb_information[2], zero_REG_MULTI_SZ_value)
+                except Exception as e:
+                    print('Error ' + usb_information[0] + ' ' + str(e))
             print('Overwritten USBSTOR subkey')
     except FileNotFoundError:
         print("Registry key USBSTOR not found")
